740edb4e5dfdef365ecca240bb1ec12a252a5323
udayveer-singh <160696428+udayveer-singh@users.noreply.github.com>
2024-02-29 10:04:09 -0600

    Create Jenkins.sh

---
+++ Jenkins.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install default-jdk -y
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update 
+sudo apt-get install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl status jenkins
